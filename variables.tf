ce9fdcaea6d2cc9aa1c5d091489ee11a865d493a
anveshreddy <anveshreddy5256@gmail.com>
2024-01-19 21:54:01 +0530

    added vpc and tags

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "access_key" {
     description = "this is access key"
     type = string
-    default = ""
+    default = "value"
   
 }
 
 variable "secret_key" {
   description = "this is secret access key id"
   type = string
-  
+  default = "value"
 }
