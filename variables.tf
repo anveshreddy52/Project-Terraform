9b55dfe5c620b8441b85529bbb67d2002f0a783d
anveshreddy <anveshreddy5256@gmail.com>
2024-01-19 21:19:31 +0530

    added some empty created files

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "access_key" {
+    description = "this is access key"
+    type = string
+    default = ""
+  
+}
+
+variable "secret_key" {
+  description = "this is secret access key id"
+  type = string
+  
+}
